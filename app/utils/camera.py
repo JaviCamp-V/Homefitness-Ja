c32f239e8a9275e5a88117b7f753268524f4378a
JaviCamp-V <54679525+JaviCamp-V@users.noreply.github.com>
2021-03-29 20:11:53 -0500

    "Webcam"

---
+++ app/utils/camera.py
@@ -0,0 +1,20 @@
+import cv2
+from imutils.video import WebcamVideoStream
+
+
+
+class WebCam(object):
+    def __init__(self,source=0):
+        self.stream=WebcamVideoStream(src=source).start()
+    def __del__(self):
+        self.stream.stop()
+    def stop():
+        self.stream.stop()
+    def get_frame(self):
+        image=self.stream.read()
+        ret,jpeg=cv2.imencode('.jpg',image)
+        data=[]
+        data.append(jpeg.tobytes())
+        return ret,data
+
+
