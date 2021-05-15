cd2804132921781ed8797599492a2c02c6be0348
Javier Stewart <54195565+JamaicanGhoul@users.noreply.github.com>
2021-05-15 01:42:34 -0500

    Update RepCounter.py

--- app/utils/RepCounter.py
+++ app/utils/RepCounter.py
@@ -102,8 +102,58 @@ class RepCounter(object):
         return self.reps
 
     def shoulderpressCounter(self,keypoints):
-        self.reps+=1
+        self.exercise = "dual"
+        if "dual" in self.exercise:
+            leftShoulder = keypoints[11][:2]
+            rightShoulder = keypoints[12][:2]
+            leftElbow = keypoints[13][:2]
+            rightElbow = keypoints[14][:2]
+            leftWrist = keypoints[15][:2]
+            rightWrist = keypoints[16][:2]
+            leftAngle = Pose.calculate_angle(leftShoulder, leftElbow, leftWrist)
+            rightAngle = Pose.calculate_angle(rightShoulder, rightElbow, rightWrist)
+            if rightAngle>=90 and leftAngle>=90 :
+                self.state="stage"
+            elif rightAngle<150 and rightAngle>90 and leftAngle<150 and leftAngle>90:
+                if self.state=="up":
+                    self.state="mid-down"
+                else:
+                    self.state="mid-up"
+            elif rightAngle<=150 and leftAngle<=150 and self.state=="mid-up":
+                self.state="up"
+                self.reps+=1
+        elif "left" in self.exercise:
+            leftWrist=keyPoints[15][:2]
+            leftElbow=keyPoints[13][:2]
+            leftShoulder=keyPoints[11][:2]
+            leftAngle=Pose.calculate_angle(leftShoulder,leftElbow,leftWrist)
+            if leftAngle>=90 :
+                self.state="stage"
+            elif leftAngle<90 and leftAngle>150:
+                if self.state=="down":
+                    self.state="mid-down"
+                else:
+                    self.state="mid-up"
+            elif leftAngle<=150 and self.state=="mid-up":
+                self.state="up"
+                self.reps+=1
+        elif "right" in self.exercise:
+            rightWrist=keyPoints[16][:2]
+            rightElbow=keyPoints[14][:2]
+            rightShoulder=keyPoints[12][:2]
+            rightAngle=Pose.calculate_angle(rightShoulder,rightElbow,rightWrist)
+            if rightAngle>=90:
+                self.state="stage"
+            elif rightAngle<90 and rightAngle>150 :
+                if self.state=="down":
+                    self.state="mid-down"
+                else:
+                    self.state="mid-up"
+            elif rightAngle<=150 and self.state=="mid-up":
+                self.state="up"
+                self.reps+=1
         return self.reps
+
 
     def resetCounter(self):
         self.reps = 0
